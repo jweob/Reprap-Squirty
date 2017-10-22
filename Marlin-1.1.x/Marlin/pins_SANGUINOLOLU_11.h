c46941128d514c3a3d801aa833534a2818058ecb
jweob <jweob@cantab.net>
2017-10-22 16:12:42 -0400

    Servo pins
    Copied across servo pin definitions

--- Marlin-1.1.x/Marlin/pins_SANGUINOLOLU_11.h
+++ Marlin-1.1.x/Marlin/pins_SANGUINOLOLU_11.h
@@ -122,6 +122,23 @@
   #define FAN_PIN           4 // Works for Panelolu2 too
 #endif
 
+// Servos
+#ifdef NUM_SERVOS
+  #define SERVO0_PIN          11
+
+  #if NUM_SERVOS > 1
+    #define SERVO1_PIN        -1
+  #endif
+
+  #if NUM_SERVOS > 2
+    #define SERVO2_PIN        -1
+  #endif
+
+  #if NUM_SERVOS > 3
+    #define SERVO3_PIN        -1
+  #endif
+#endif
+
 //
 // Misc. Functions
 //
